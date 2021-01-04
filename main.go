8688e6fadf4fd2133d4bb83a14d6c139bc110958
Madiyar <wampir_drakula@mail.ru>
2021-01-04 18:50:00 +0600

    routing added

--- main.go
+++ main.go
@@ -6,12 +6,21 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Snippetbox"))
+	w.Write([]byte("Hello from Gladosov"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display  a specific  snippet..."))
+}
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create new snippet..."))
 }
 func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
