595261a5049d3c68f7262dd603f7c1c97bb1a90c
Madiyar <wampir_drakula@mail.ru>
2021-01-04 19:01:20 +0600

    http status code done

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Gladosov"))
 }
 
@@ -13,8 +18,15 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display  a specific  snippet..."))
 }
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+
+		w.WriteHeader(405)
+		w.Write([]byte("Method Not Allowed"))
+		return
+	}
 	w.Write([]byte("Create new snippet..."))
 }
+
 func main() {
 
 	mux := http.NewServeMux()
